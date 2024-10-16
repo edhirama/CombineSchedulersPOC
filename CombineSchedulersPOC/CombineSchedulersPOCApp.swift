e67ef920b272ab6d156dc4773ca8659e41c052f3
Edgar Hirama <edgarh@airtm.io>
2024-10-15 21:31:24 -0300

    Initial Commit

---
+++ CombineSchedulersPOC/CombineSchedulersPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineSchedulersPOCApp.swift
+//  CombineSchedulersPOC
+//
+//  Created by Edgar Hirama on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineSchedulersPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
